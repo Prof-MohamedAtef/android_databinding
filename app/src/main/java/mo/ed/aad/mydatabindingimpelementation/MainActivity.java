65f77ee3218a6a893882c91c421d802f443fff3e
Prof-Mohamed Atef <proofesser@gmail.com>
2020-05-02 03:23:43 +0200

    binding views on activity

--- app/src/main/java/mo/ed/aad/mydatabindingimpelementation/MainActivity.java
+++ app/src/main/java/mo/ed/aad/mydatabindingimpelementation/MainActivity.java
@@ -3,12 +3,34 @@ package mo.ed.aad.mydatabindingimpelementation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import mo.ed.aad.mydatabindingimpelementation.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
+
+    int counter =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view=activityMainBinding.getRoot();
+        setContentView(view);
+
+        activityMainBinding.button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                activityMainBinding.tv1.setText("");
+                activityMainBinding.tv1.setText("Counter Value = ");
+                activityMainBinding.tv1.append(String.valueOf(counter));
+            }
+        });
+
+
+
     }
 }
